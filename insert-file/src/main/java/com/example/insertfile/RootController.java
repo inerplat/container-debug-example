c487e5080780595af1fc56ba83a7f67f058d04e7
DH Kim <inerplat@gmail.com>
2022-04-26 01:16:25 +0900

    add insert-file example

---
+++ insert-file/src/main/java/com/example/insertfile/RootController.java
@@ -0,0 +1,35 @@
+package com.example.insertfile;
+
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/{fileName}")
+    public String getFile(@PathVariable String fileName) {
+        File file = new File("/file/" + fileName);
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fileInputStream);
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = isr.read()) != -1) {
+                sb.append((char) c);
+            }
+            return sb.toString();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
